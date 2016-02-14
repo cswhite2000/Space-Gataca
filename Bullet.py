4b0375daba15b00ad3e484a9df14244c0d94d108
alvintzheng <alvintz@yahoo.com>
2016-02-13 18:50:39 -0800

    Update Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -19,3 +19,25 @@ class Bullet:
         self.y=self.y+5
     def tick(self):
         self.y=self.y-5
+    def collide(self, other):        
+        otherRec = other.getRec()
+        otherX = otherRec[0]
+        otherY = otherRec[1]
+        otherWidth = otherRec[2]
+        otherHeight = otherRec[3]
+        myRec = self.getRec()        
+        width = myRec[0] + myRec[2] 
+        height = myRec[1] + myRec[3]                
+        if self.x > (otherX + otherWidth):
+            return False               
+        elif width < otherX:
+            return False                         
+        elif self.y > (otherY + otherHeight):
+            return False                         
+        elif height < otherY:
+            return False 
+        else:
+            return True
+    def getRec(self):
+        myRec = self.img.get_rect()
+        return (self.x, self.y, myRec[2], myRec[3])
