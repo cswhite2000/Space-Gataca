d62862a6225a1118139207b05e48859ea9123ef4
alvintzheng <alvintz@yahoo.com>
2016-02-13 17:24:58 -0800

    Create Bullet.py

---
+++ Bullet.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+
+class Bullet:
+    def __init__ (self, newX, newY):
+        self.x = newX
+        self.y = newY
+        
+        self.img = pygame.image.load("Images/Bullet.png")
+    def draw(self, window):
+        window.blit(self.img, (self.x,self.y))
+    def moveLeft(self):
+        self.x=self.x-5
+    def moveRight(self):
+        self.x=self.x+5
+    def moveUp(self):
+        self.y=self.y-5
+    def moveDown(self):
+        self.y=self.y+5
+    def tick(self):
+        self.y=self.y-5
