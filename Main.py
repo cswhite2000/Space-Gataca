68f39b7f6bd50861584817db576327a655d94d38
alvintzheng <alvintz@yahoo.com>
2016-02-13 17:25:44 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@
 import pygame,sys,math
 from pygame.locals import *
 from Ship import *
-
+from Bullets import *
+from Bullet import *
 
 pygame.init()
 pygame.key.set_repeat(1,1)
@@ -10,10 +11,12 @@ screen = pygame.display.set_mode((800,600))
 background = pygame.Surface((800,600))
 
 ship= Ship(100,100)
+bullets= Bullets()
 
 while True:# Game loop
     screen.blit(background,(0,0))
     ship.draw(screen)
+    bullets.draw(screen)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type==QUIT or (event.type==KEYUP and event.key==K_ESCAPE):
@@ -28,4 +31,6 @@ while True:# Game loop
                 ship.moveLeft()
             elif event.key==K_RIGHT:
                 ship.moveRight()
+            elif event.key==K_SPACE:
+                bullets.addNew(ship.x,ship.y)
     pygame.time.wait(10)
