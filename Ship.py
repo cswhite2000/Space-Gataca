87d4db1a4bc785f3e034054a4b0bada973a66ca2
alvintzheng <alvintz@yahoo.com>
2016-02-13 18:49:40 -0800

    Update Ship.py

--- Ship.py
+++ Ship.py
@@ -5,14 +5,41 @@ class Ship:
     def __init__ (self, newX, newY):
         self.x = newX
         self.y = newY
-        self.img = pygame.image.load("https://cloud.githubusercontent.com/assets/15203641/13030928/2a482cb0-d26f-11e5-80a5-67f7e2bee92e.png")
+        
+        self.img = pygame.image.load("Images/Ship.png")
     def draw(self, window):
         window.blit(self.img, (self.x,self.y))
     def moveLeft(self):
-        self.x=self.x-5
+        if self.x>0:
+            self.x=self.x-5
     def moveRight(self):
-        self.x=self.x+5
+        if self.x<720:
+            self.x=self.x+5
     def moveUp(self):
-        self.y=self.y-5
+        if self.y>0:
+            self.y=self.y-5
     def moveDown(self):
-        self.y=self.y+5
+        if self.y<520:
+            self.y=self.y+5
+    def collide(self, other):        
+        otherRec = other.getRec()
+        otherX = otherRec[0]
+        otherY = otherRec[1]
+        otherWidth = otherRec[2]
+        otherHeight = otherRec[3]
+        myRec = self.getRec()        
+        width = myRec[0] + myRec[2] 
+        height = myRec[1] + myRec[3]                
+        if self.x > (otherX + otherWidth):
+            return False               
+        elif width < otherX:
+            return False                         
+        elif self.y > (otherY + otherHeight):
+            return False                         
+        elif height < otherY:
+            return False 
+        else:
+            return True
+    def getRec(self):
+        myRec = self.img.get_rect()
+        return (self.x, self.y, myRec[2], myRec[3])
