2431ada88fb9e2c73217970caa07821eb53c01c6
alvintzheng <alvintz@yahoo.com>
2016-02-13 16:36:54 -0800

    Update Ship.py

--- Ship.py
+++ Ship.py
@@ -5,7 +5,7 @@ class Ship:
     def __init__ (self, newX, newY):
         self.x = newX
         self.y = newY
-        self.img = pygame.image.load("https://cloud.githubusercontent.com/assets/15203641/13030928/2a482cb0-d26f-11e5-80a5-67f7e2bee92e.png")
+        self.img = pygame.image.load("Images/Ship.png")
     def draw(self, window):
         window.blit(self.img, (self.x,self.y))
     def moveLeft(self):
